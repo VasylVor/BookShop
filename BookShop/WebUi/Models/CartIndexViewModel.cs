b9df6c346c5e28e01e6d44993782fd5f59396cc3
Vasyl Voronin <vasilvoronin0@gmail.com>
2018-08-28 17:55:02 +0300

    Cart

---
+++ BookShop/WebUi/Models/CartIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DomainBookShop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUi.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
